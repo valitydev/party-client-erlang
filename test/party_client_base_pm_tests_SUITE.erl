590dd7fe4088784b96b4e32ed4de3577144275a2
kehitt <kehitt@users.noreply.github.com>
2022-04-07 00:29:30 +0300

    fix formatting

--- test/party_client_base_pm_tests_SUITE.erl
+++ test/party_client_base_pm_tests_SUITE.erl
@@ -210,7 +210,8 @@ shop_operations_test(C) ->
     ok = party_client_thrift:activate_shop(PartyId, ShopId, Client, Context),
     ok = party_client_thrift:block_shop(PartyId, ShopId, <<"block_test">>, Client, Context),
     ok = party_client_thrift:unblock_shop(PartyId, ShopId, <<"unblock_test">>, Client, Context),
-    {ok, #domain_ShopAccount{settlement = AccountID}} = party_client_thrift:get_shop_account(PartyId, ShopId, Client, Context),
+    {ok, #domain_ShopAccount{settlement = AccountID}} =
+        party_client_thrift:get_shop_account(PartyId, ShopId, Client, Context),
     {ok, _ShopAccount} = party_client_thrift:get_account_state(PartyId, AccountID, Client, Context).
 
 -spec claim_operations_test(config()) -> any().
